a1df7164dca9d4802a58aa64442e717784f0ef62
= <=>
2020-11-10 16:10:53 -0300

    Listando Categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
   baseUrl: "http://localhost:8080",
+  bucketBaseUrl: "https://app-springboot-ionic.s3-sa-east-1.amazonaws.com/imagens+das+categorias+app-spring-ionic",
 };
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,13 +1,8 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { API_CONFIG } from "../../config/api.config";
+import { CategoriaDTO } from "../../models/categoria.dto";
 import { CategoriaService } from "../../services/domain/categoria.service";
-
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,6 +10,9 @@ import { CategoriaService } from "../../services/domain/categoria.service";
   templateUrl: "categorias.html",
 })
 export class CategoriasPage {
+  bucketUrl = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -24,7 +22,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll().subscribe(
       (response) => {
-        console.log(response);
+        this.items = response;
       },
       (error) => {
         console.log(error);
