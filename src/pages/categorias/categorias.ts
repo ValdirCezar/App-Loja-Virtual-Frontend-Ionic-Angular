bf98c855e6970dd565da1e3e2a529ab2698a1e5b
= <=>
2020-11-11 13:40:56 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -24,9 +24,7 @@ export class CategoriasPage {
       (response) => {
         this.items = response;
       },
-      (error) => {
-        console.log(error);
-      }
+      (error) => {}
     );
   }
 }
