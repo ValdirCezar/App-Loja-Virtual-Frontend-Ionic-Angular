eaf82f44c8a455706394a60fa036edcbeedc63e0
= <=>
2020-11-13 13:51:41 -0300

    Botão de logout no menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from "@angular/core";
 import { Nav, Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+import { AuthService } from "../services/auth.service";
 
 @Component({
   templateUrl: "app.html",
@@ -16,7 +17,8 @@ export class MyApp {
   constructor(
     public platform: Platform,
     public statusBar: StatusBar,
-    public splashScreen: SplashScreen
+    public splashScreen: SplashScreen,
+    public auth: AuthService
   ) {
     this.initializeApp();
 
@@ -24,6 +26,7 @@ export class MyApp {
     this.pages = [
       { title: "Profile", component: "ProfilePage" },
       { title: "Categorias", component: "CategoriasPage" },
+      { title: "Logout", component: "" },
     ];
   }
 
@@ -36,9 +39,15 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  openPage(page: { title: string; component: string }) {
+    switch (page.title) {
+      case "Logout":
+        this.auth.logout();
+        this.nav.setRoot("HomePage");
+        break;
+
+      default:
+        this.nav.setRoot(page.component);
+    }
   }
 }
