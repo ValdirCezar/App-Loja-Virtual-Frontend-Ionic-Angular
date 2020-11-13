ade4f6eb752913d35af236745412f12beaacc9cb
= <=>
2020-11-13 10:57:33 -0300

    Mostrando dados e imagem do cliente na página de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { API_CONFIG } from "../../config/api.config";
+import { ClienteDTO } from "../../models/cliente.dto";
+import { ClienteService } from "../../services/domain/cliente.service";
 import { StorageService } from "../../services/storage.service";
 
 @IonicPage()
@@ -8,18 +11,35 @@ import { StorageService } from "../../services/storage.service";
   templateUrl: "profile.html",
 })
 export class ProfilePage {
-  email: string;
+  cliente: ClienteDTO;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public storage: StorageService
+    public storage: StorageService,
+    public clienteService: ClienteService
   ) {}
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
-      this.email = localUser.email;
+      this.clienteService.findByEmail(localUser.email).subscribe(
+        (response) => {
+          this.cliente = response;
+          this.getImageIfExists();
+        },
+        (error) => {}
+      );
     }
+  }
+
+  // buscar imagem do s3
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id).subscribe(
+      (response) => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+      (error) => {}
+    );
   }
 }
