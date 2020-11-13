64eb0f241fa7db742206122e26c3d68924a0d6d3
= <=>
2020-11-13 11:37:55 -0300

    Tratando error 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -7,9 +7,13 @@ import {
 } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(public storage: StorageService){}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
@@ -27,8 +31,18 @@ export class ErrorInterceptor implements HttpInterceptor {
       console.log("Erro detectado pelo interceptor");
       console.log(errorObj);
 
+      switch (errorObj.status) {
+        case 403:
+          this.heandle403();
+          break;
+      }
+
       return Observable.throw(errorObj);
     }) as any;
+  }
+
+  heandle403() {
+    this.storage.setLocalUser(null);
   }
 }
 
